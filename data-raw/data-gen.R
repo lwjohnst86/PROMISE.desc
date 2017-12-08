c0f3b5eaf1420b08c80406661178e1f91463b119
Luke W Johnston <lwjohnst@gmail.com>
2017-12-08 13:06:09 -0500

    filter out small VN in example dataset

--- data-raw/data-gen.R
+++ data-raw/data-gen.R
@@ -1,6 +1,7 @@
 library(dplyr)
 example_data <- PROMISE.data::PROMISE %>%
     select(SID, VN, DM, IFG, IGT) %>%
+    filter(VN %in% c(1, 3, 6)) %>%
     sample_frac(.5, replace = TRUE)
 
 devtools::use_data(example_data, overwrite = TRUE)
